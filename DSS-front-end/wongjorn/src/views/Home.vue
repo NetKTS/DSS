737a4a9cc8fb77fd2e73e52729b62566d66e6256
GUYGAM <guygam31@gmail.com>
2021-03-25 23:47:06 +0700

    update image

--- DSS-front-end/wongjorn/src/views/Home.vue
+++ DSS-front-end/wongjorn/src/views/Home.vue
@@ -1,39 +1,38 @@
 <template>
-  <div class="bgcolor"><router-link to="/form">
-    <div class="backgroundimage">
-      <div class="setmid" ><img src="https://media1.giphy.com/media/24G0F8lQWYMb6jD47X/giphy.gif" alt="this slowpoke moves"  width=250/></div>
-    </div></router-link>
-  </div>
-  
+  <body>
+    <router-link to="/form">
+      <center>
+          <img
+            style="width:30%;margin-top:7%;opacity:1;"
+            id="img1"
+            src="../assets/wongjornlogo2.png"
+          />
+      </center>
+    </router-link>
+  </body>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'Home',
-  components: {
-    
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
 
 <style>
-.backgroundimage{
-  background-image: url('../assets/wongjorn.png');
-  background-size: 100vw;
-  background-repeat: no-repeat;
-  height: 100vh;
-  
-}
-.bgcolor{
+/* .backgroundimage{
+  background-image: url('../assets/wongjornlogo2.png');
+  height: 100%;
+  margin-top: 6%;
+  opacity: 1;
+} */
+body {
   background-color: #efede8;
-  height: 100vh;
 }
-.setmid{
+
+.setmid {
   margin-left: 30%;
 }
-
-
 </style>
