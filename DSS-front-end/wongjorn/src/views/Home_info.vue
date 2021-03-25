409a87c952e3d211db7ac2d4efc1ea34bb18d31b
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2021-03-25 23:47:53 +0700

    add home_info naja

---
+++ DSS-front-end/wongjorn/src/views/Home_info.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bgcolor"><router-link to="/form">
+    <div class="backgroundimage">
+      <div class="setmid" ><img src="https://media1.giphy.com/media/24G0F8lQWYMb6jD47X/giphy.gif" alt="this slowpoke moves"  width=250/></div>
+    </div></router-link>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+
+</style>
